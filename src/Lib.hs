5b1671eef21da4263525498dce2f8ad18ae9dc2c
pml76 <peter.lennartz@gmail.com>
2024-08-02 20:33:44 +0200

    .

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,28 @@
 module Lib
-    ( someFunc
+    ( someFunc,
+      priceEuropeanCall,
+      Strike(..),
+      Underlying(..),
+      Price(..)
     ) where
+
+import qualified Statistics.Distribution.Normal as Normal
+import qualified Data.Time.Clock as Clock
+
+newtype Strike = Strike Double
+newtype Underlying = Underlying Double
+newtype Price = Price Double
+newtype InterestRate = InterestRate Double
+
+priceEuropeanCall :: Normal.NormalDistribution -> Clock.NominalDiffTime -> Strike -> Underlying -> InterestRate -> Price
+priceEuropeanCall distr diffTime strike underlying interestRate =
+  let (Strike k) = strike
+      (Underlying s) = underlying
+      (InterestRate r) = interestRate
+      d1 = log (s/k)
+   in Price d1
+
+
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
