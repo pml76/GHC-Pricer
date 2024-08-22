f37f5b70dd2502b5a96e40325694e367a880246c
pml76 <peter.lennartz@gmail.com>
2024-08-22 17:10:19 +0200

    .

--- src/Lib.hs
+++ src/Lib.hs
@@ -10,9 +10,19 @@ import qualified Statistics.Distribution as SD
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
-calcPrice :: (Double -> Double) -> Double
-calcPrice payout = 
-    let f x         = (payout x) * SD.normal  
+newtype InterestRate = InterestRate Double
+newtype Price = Price Double
+
+class Market a where
+    interestRate :: a -> InterestRate
+    underlying   :: a -> Price
+    logDensity :: a -> Double -> Double
+    timeToMaturity :: a -> Double
+    volatility :: a -> Double
+
+calcPrice :: Market m => m -> (Double -> Double) -> Double
+calcPrice m payout = 
+    let f y         = underlying m * exp (timeToMaturity m * interestRate m - volatility m * sqrt (timeToMaturity m) * y) 
         r           = NI.nonNegative NI.trap f
         (low, high) = (NI.confidence . NI.absolute 6.0e-10) r
     in (low + high) / 2
