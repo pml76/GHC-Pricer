d153b077a6252ebd5bab0f39af4441c51be21963
pml76 <peter.lennartz@gmail.com>
2024-08-22 19:53:34 +0200

    ,

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,25 +5,40 @@ module Lib
     ) where
 
 import qualified Numeric.Integration.TanhSinh as NI
-import qualified Statistics.Distribution as SD
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
 newtype InterestRate = InterestRate Double
 newtype Price = Price Double
+newtype Volatility = Volatility Double
+
+priceToDouble :: Price -> Double 
+priceToDouble (Price d) = d 
+
+interestRateToDouble :: InterestRate -> Double
+interestRateToDouble (InterestRate d) = d
+
+volatilityToDouble :: Volatility -> Double
+volatilityToDouble (Volatility d) = d 
+
+
+
 
 class Market a where
     interestRate :: a -> InterestRate
     underlying   :: a -> Price
-    logDensity :: a -> Double -> Double
     timeToMaturity :: a -> Double
-    volatility :: a -> Double
+    volatility :: a -> Volatility
 
 calcPrice :: Market m => m -> (Double -> Double) -> Double
 calcPrice m payout = 
-    let f y         = underlying m * exp (timeToMaturity m * interestRate m - volatility m * sqrt (timeToMaturity m) * y) 
-        r           = NI.nonNegative NI.trap f
-        (low, high) = (NI.confidence . NI.absolute 6.0e-10) r
+    let r           = (interestRateToDouble . interestRate) m
+        x           = (priceToDouble . underlying) m
+        tau         = timeToMaturity m
+        sigma       = (volatilityToDouble . volatility) m
+        f y         = x * exp (tau * r - sigma * sqrt tau * y ) 
+        r2           = NI.nonNegative NI.trap f
+        (low, high) = (NI.confidence . NI.absolute 6.0e-10) r2
     in (low + high) / 2
              
