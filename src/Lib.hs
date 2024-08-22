cfbb68c0c56ff1eecbe8e6afc11748bfb6e47cf7
pml76 <peter.lennartz@gmail.com>
2024-08-22 20:40:27 +0200

    .

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,8 @@
+{-# LANGUAGE BangPatterns #-}
+
 module Lib
     ( someFunc
-    , calcPrice
+    , Market(..)
 
     ) where
 
@@ -13,14 +15,14 @@ newtype InterestRate = InterestRate Double
 newtype Price = Price Double
 newtype Volatility = Volatility Double
 
-priceToDouble :: Price -> Double 
-priceToDouble (Price d) = d 
+priceToDouble :: Price -> Double
+priceToDouble (Price d) = d
 
 interestRateToDouble :: InterestRate -> Double
 interestRateToDouble (InterestRate d) = d
 
 volatilityToDouble :: Volatility -> Double
-volatilityToDouble (Volatility d) = d 
+volatilityToDouble (Volatility d) = d
 
 
 
@@ -28,17 +30,15 @@ volatilityToDouble (Volatility d) = d
 class Market a where
     interestRate :: a -> InterestRate
     underlying   :: a -> Price
-    timeToMaturity :: a -> Double
     volatility :: a -> Volatility
 
-calcPrice :: Market m => m -> (Double -> Double) -> Double
-calcPrice m payout = 
-    let r           = (interestRateToDouble . interestRate) m
-        x           = (priceToDouble . underlying) m
-        tau         = timeToMaturity m
-        sigma       = (volatilityToDouble . volatility) m
-        f y         = x * exp (tau * r - sigma * sqrt tau * y ) 
-        r2           = NI.nonNegative NI.trap f
-        (low, high) = (NI.confidence . NI.absolute 6.0e-10) r2
-    in (low + high) / 2
-             
+    optionPrice :: a -> (Double -> Double) -> Double -> Double
+    optionPrice m payout tau =
+        let !r           = (interestRateToDouble . interestRate) m
+            !x           = (priceToDouble . underlying) m
+            !sigma       = (volatilityToDouble . volatility) m
+            f y         = x * exp ( - (tau * r) ) * (payout . exp) ( - (sigma * sqrt tau * y) + (r-sigma*sigma/2)*tau) * exp (- (y * y / 2))
+            !result      = NI.everywhere NI.trap f
+            (!low, !high) = (NI.confidence . NI.absolute 6.0e-10) result
+        in (low + high) / 2
+
